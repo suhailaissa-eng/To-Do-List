c09207e2b10cf555782a8e0c28541b3fe794c524
suhaila <sosoissa04@gmail.com>
2025-01-28 21:57:31 +0200

    create files

---
+++ IdeaProjects/ToDoooList/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.todooolist {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.todooolist to javafx.fxml;
+    exports com.example.todooolist;
+}
